4536b10ed09d6561638c864292c5c34f9e6df33b
donovanCarreno <donovan_carreno@yahoo.com>
2017-12-26 19:34:37 -0600

    setup startRemoveExpense

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 
 import ExpenseForm from './ExpenseForm'
 
-import { editExpense, removeExpense } from '../actions/expenses'
+import { editExpense, startRemoveExpense } from '../actions/expenses'
 
 // export for testing
 export class EditExpensePage extends Component {
@@ -13,7 +13,7 @@ export class EditExpensePage extends Component {
   }
 
   onClick = () => {
-    this.props.removeExpense({ id: this.props.expense.id })
+    this.props.startRemoveExpense({ id: this.props.expense.id })
     this.props.history.push('/')
   }
 
@@ -36,7 +36,7 @@ const mapStateToProps = (state, props) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   editExpense: (id, expense) => dispatch(editExpense(id, expense)),
-  removeExpense: (id) => dispatch(removeExpense(id))
+  startRemoveExpense: (id) => dispatch(startRemoveExpense(id))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage)
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,6 @@
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
-import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses } from '../../actions/expenses'
+import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses, startRemoveExpense } from '../../actions/expenses'
 import expenses from '../fixtures/expenses'
 import database from '../../firebase/firebase'
 
@@ -103,6 +103,24 @@ test('should fetch the expenses from firebase', (done) => {
         type: 'SET_EXPENSES',
         expenses
       })
+      done()
+    })
+})
+
+test('should remove expenses from firebase', (done) => {
+  const store = createMockStore({})
+  const { id } = expenses[2]
+  store.dispatch(startRemoveExpense({ id }))
+    .then(() => {
+      const actions = store.getActions()
+      expect(actions[0]).toEqual({
+        type: 'REMOVE_EXPENSE',
+        id
+      })
+      return database.ref(`expenses/${id}`).once('value')
+    })
+    .then((snapshot) => {
+      expect(snapshot.val()).toBeFalsy()
       done()
     })
 })

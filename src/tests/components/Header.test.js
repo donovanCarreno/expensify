d387687d9b57ef64ce9a0c4f1a89c31a8c5a8165
donovanCarreno <donovan_carreno@yahoo.com>
2017-12-27 10:19:38 -0600

    add private routes

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 // import ReactShallowRenderer from 'react-test-renderer/shallow'
-import Header from '../../components/Header'
+import { Header } from '../../components/Header'
+
+let wrapper, startLogout
+
+beforeEach(() => {
+  startLogout = jest.fn()
+  wrapper = shallow(<Header startLogout={startLogout}/>)
+})
 
 test('should render Header correctly', () => {
   // const renderer = new ReactShallowRenderer()
   // renderer.render(<Header />)
   //
   // expect(renderer.getRenderOutput()).toMatchSnapshot()
-  const wrapper = shallow(<Header />)
   // expect(wrapper.find('h1').text()).toBe('Expensify')
   expect(wrapper).toMatchSnapshot()
+})
+
+test('should call startLogout on button click', () => {
+  wrapper.find('button').simulate('click')
+  expect(startLogout).toHaveBeenCalled()
 })
